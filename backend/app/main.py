12ee88f27564871c74f64dadb0c2fd6d8d308df6
ciire <chueric9@gmail.com>
2025-05-06 21:52:32 -0500

    Connected and retrieved product data from rainforest API and added redis cache

---
+++ backend/app/main.py
@@ -0,0 +1,44 @@
+import json
+from flask import Flask, jsonify, request
+import os
+from flask_cors import CORS
+import redis
+from datetime import timedelta
+from lambda_layer.rainforest_api import search_amazon_products
+app = Flask(__name__)
+# Allow CORS from frontend dev server
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+
+# Configure Redis
+redis_client = redis.Redis(
+    host=os.getenv('REDIS_HOST', 'localhost'),
+    port=int(os.getenv('REDIS_PORT', 6379)),
+    password=os.getenv('REDIS_PASSWORD', None),
+    db=0
+)
+
+@app.route("/api/hello")
+def api_hello():
+    return {"message": "Hello from Python backend"}
+
+@app.route("/api/search")
+def api_search():
+    search_term = request.args.get("search")
+    if not search_term:
+        return {"error": "Missing search term"}, 400
+
+    cache_key = f"search:{search_term}"
+    cached_results = redis_client.get(cache_key)
+
+    if cached_results:
+        return jsonify(json.loads(cached_results.decode('utf-8')))
+
+    try:
+        results = search_amazon_products(search_term)
+        redis_client.setex(cache_key, timedelta(hours=1), json.dumps(results))
+        return jsonify(results)
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ backend/lambda_layer/rainforest_api.py
@@ -0,0 +1,43 @@
+import requests
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from requests.exceptions import RequestException, Timeout, JSONDecodeError
+
+load_dotenv()
+
+API_KEY = os.getenv('API_RAINFOREST_KEY')
+print(f"API Key: {os.getenv('API_RAINFOREST_KEY')}")
+def search_amazon_products(search_term):
+    try:
+        url = 'https://api.rainforestapi.com/request'
+
+        params= {
+            'api_key': API_KEY,
+            'type': 'search',
+            'amazon_domain': 'amazon.com',
+            'search_term': search_term,
+            
+        }
+
+
+        response = requests.get(url, params=params)
+
+        # print(f"Rainforest response status: {response.status_code}")
+        # print(f"Rainforest response body: {response.text}")
+
+        response.raise_for_status()  # Will throw if 4xx/5xx
+        data = response.json()
+        
+        results = []
+        for product in data.get("search_results", []):
+            title = product['title']
+            price = product.get('price', {}).get('value')
+            results.append((title, price))
+        return results
+    except JSONDecodeError:
+            return {"error": "Invalid API response format"}, 502
+    except RequestException as e:
+        return {"error": f"API request failed: {str(e)}"}, 502
+
+
